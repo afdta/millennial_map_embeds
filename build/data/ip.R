a350611df489a9ad9b43d9c44575b09e5b5b4315
Alec Friedhoff <afriedhoff@gmail.com>
2018-01-18 00:46:25 -0500

    data processing check done

--- build/data/ip.R
+++ build/data/ip.R
@@ -5,9 +5,9 @@ library(jsonlite)
 
 pop <- read_xls("/home/alec/Projects/Brookings/millenials/build/data/Millennial Report_Appendices nw.xls", 
                 sheet="BGTable A", range="B10:H109", col_names=FALSE)[seq(1,7,2)]
-names(pop) <- c("Metro", "MPop15", "MShare15", "YAGrowth1015")
+names(pop) <- c("Metro_", "MPop15", "MShare15", "YAGrowth1015")
 
-pop$Metro <- sub("Nashville-Davidson--Murfreesboro--Franklin, TN", "Nashville-Davidson-Murfreesboro-Franklin, TN", pop$Metro)
+pop$Metro <- sub("Nashville-Davidson--Murfreesboro--Franklin, TN", "Nashville-Davidson-Murfreesboro-Franklin, TN", pop$Metro_)
 
 race <- read_xls("/home/alec/Projects/Brookings/millenials/build/data/Millennial Report_Appendices nw.xls", 
                  sheet="BGTable C", range="B10:J109", col_names=FALSE)[c(1,3:9)]
@@ -27,6 +27,6 @@ mets$CBSA_Title <- gsub("--", "-", mets$CBSA_Title)
 
 metro_data <- inner_join(mets, merged, by=c("CBSA_Title"="Metro"))
 
-json <- toJSON(metro_data, digits=5, na="null")
+json <- toJSON(metro_data[-3], digits=5, na="null")
 
 write_lines(json, "/home/alec/Projects/Brookings/millenials/assets/millenials_data.json")
